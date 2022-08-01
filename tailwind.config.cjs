35ac7ef1cef3ea57e91cf115c47dd7863089cbde
glubdub <gabrielferreira5247@gmail.com>
2022-08-01 08:32:52 -0300

    Installing tailwindcss

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    colors: {
+      bg: '#282A3B',
+      blue: '#0FD9ED',
+      red: '#F84F60',
+      gray: '#757484'
+    },
+  },
+  plugins: []
+};
